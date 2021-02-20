e03a5ffc24be1fdb2d19bfd7e783836cad989325
Howardnm <55622355+Howardnm@users.noreply.github.com>
2021-02-20 21:20:24 +0800

    Update v2rayinstall.sh

--- v2rayinstall.sh
+++ v2rayinstall.sh
@@ -4,10 +4,9 @@ basepath=$(cd `dirname $0`; pwd)
 
 read -t 30 -p "是否安装v2ray? y or n :" isMove
 if [ "${isMove}" = "y" ]; then
-cd /etc/v2ray
-get https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp_detour.json 
-get https://raw.githubusercontent.com/Howardnm/vtworay/main/config_tcp.json 
-get https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp.json
+curl -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp_detour.json 
+curl -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/config_tcp.json 
+curl -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp.json
 docker run -d --name v2ray_mkcp --restart=always -v /etc/v2ray:/etc/v2ray -p 52001:52001 -p 52001:52001/udp v2fly/v2fly-core  v2ray -config=/etc/v2ray/config_mkcp.json
 docker run -d --name v2ray_tcp --restart=always -v /etc/v2ray:/etc/v2ray -p 51001:51001 -p 51001:51001/udp v2fly/v2fly-core  v2ray -config=/etc/v2ray/config_tcp.json
 elif [ "${isMove}" = "n" ]; then
