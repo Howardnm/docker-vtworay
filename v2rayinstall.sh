e4d005acf3e29b3d0746e23bbd24b8a975a5071d
Howardnm <55622355+Howardnm@users.noreply.github.com>
2021-02-20 21:07:24 +0800

    Update v2rayinstall.sh

--- v2rayinstall.sh
+++ v2rayinstall.sh
@@ -4,6 +4,8 @@ basepath=$(cd `dirname $0`; pwd)
 
 read -t 30 -p "是否安装v2ray? y or n :" isMove
 if [ "${isMove}" = "y" ]; then
-wget -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp_detour.json https://raw.githubusercontent.com/Howardnm/vtworay/main/config_tcp.json https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp.json
+wget -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp_detour.json 
+wget -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/config_tcp.json 
+wget -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp.json
 docker run -d --name v2ray_mkcp --restart=always -v /etc/v2ray:/etc/v2ray -p 52001:52001 -p 52001:52001/udp v2fly/v2fly-core  v2ray -config=/etc/v2ray/config_mkcp.json
 docker run -d --name v2ray_tcp --restart=always -v /etc/v2ray:/etc/v2ray -p 51001:51001 -p 51001:51001/udp v2fly/v2fly-core  v2ray -config=/etc/v2ray/config_tcp.json
