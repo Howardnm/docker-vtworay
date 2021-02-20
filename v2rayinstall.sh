105a75923ec02a8fb187749ffd945b256c013787
Howardnm <55622355+Howardnm@users.noreply.github.com>
2021-02-20 22:49:56 +0800

    Update v2rayinstall.sh

--- v2rayinstall.sh
+++ v2rayinstall.sh
@@ -10,16 +10,13 @@ mkdir /etc/v2ray
         curl -sSL https://get.daocloud.io/docker | sh
         systemctl start docker
         systemctl enable docker
-        echo "安装docker环境...安装完成!"
+        echo "${Red_font_prefix}安装docker环境...安装完成!${Font_color_suffix}"
 	echo "-----------------------------------------"
 	echo "-----------------------------------------"
     fi
-read -t 30 -p "输入：\
-1、安装v2ray ssr ss\
-2、卸载v2ray ssr ss\
-3、退出 :" howard
+read -t 30 -p "${Red_font_prefix}输入：（1）安装v2ray ssr ss （2）卸载v2ray ssr ss （3）退出${Font_color_suffix} :" howard
 if [ "${howard}" = "1" ]; then
-	read -t 30 -p "是否安装v2ray? y or n :" vtworay
+	read -t 30 -p "是否安装v2ray${Font_color_suffix}? y or n :" vtworay
 	if [ "${vtworay}" = "y" ]; then
 		rm -f /etc/v2ray/*
 		wget -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp_detour.json 
@@ -30,7 +27,7 @@ if [ "${howard}" = "1" ]; then
 	elif [ "${vtworay}" = "n" ]; then
 		echo "---"
 	fi
-	read -t 30 -p "是否安装ss和ssr? y or n :" ssrss
+	read -t 30 -p "${Red_font_prefix}是否安装ss和ssr${Font_color_suffix}? y or n :" ssrss
 	if [ "${ssrss}" = "y" ]; then
 		docker run -d -p 54019:8388 -p 54019:8388/udp --name shadowsocks-libev --restart=always -e SERVER_PORT="8388" -e PASSWORD="hxc123" -e METHOD="aes-128-gcm" -e DNS_ADDRS="8.8.8.8,223.5.5.5,8.8.4.4,114.114.114.114" -e TIMEOUT="3000" shadowsocks/shadowsocks-libev
 		docker run -d -p 54009:8388 -p 54009:8388/udp --name shadowsocksR-libev --restart=always -e SERVER_PORT="8388" -e PASSWORD="hxc123" -e METHOD="aes-128-cfb" -e DNS_ADDRS="8.8.8.8,223.5.5.5,8.8.4.4,114.114.114.114" -e TIMEOUT="3000" shadowsocks/shadowsocks-libev
@@ -38,37 +35,37 @@ if [ "${howard}" = "1" ]; then
 		echo "---"
 	fi
 	if [ "${vtworay}" = "y" ]; then
-		echo "订阅后，修改ip即可使用"
+		echo "${Red_font_prefix}订阅后，修改ip即可使用${Font_color_suffix}"
 		echo "vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogInYycmF5IiwNCiAgImFkZCI6ICIwLjAuMC4wIiwNCiAgInBvcnQiOiAiNTIwMDEiLA0KICAiaWQiOiAiOWFjOWEwYjMtZTgwYi00NjZiLTgxM2EtNzM4YzZjZmQ0NmNkIiwNCiAgImFpZCI6ICIyMCIsDQogICJuZXQiOiAia2NwIiwNCiAgInR5cGUiOiAibm9uZSIsDQogICJob3N0IjogIiIsDQogICJwYXRoIjogIiIsDQogICJ0bHMiOiAiIg0KfQ=="
 	elif [ "${vtworay}" = "n" ]; then
 		echo "---"
 	fi
 	if [ "${ssrss}" = "y" ]; then
-		echo "订阅后，修改ip即可使用"
+		echo "${Red_font_prefix}订阅后，修改ip即可使用${Font_color_suffix}"
 		echo "ss://YWVzLTEyOC1nY206aHhjMTIzQDAuMC4wLjA6NTQwMTk#ss"
 		echo "ssr://MC4wLjAuMDo1NDAwOTpvcmlnaW46YWVzLTEyOC1jZmI6cGxhaW46YUhoak1USXovP3JlbWFya3M9YzNOeSZwcm90b3BhcmFtPSZvYmZzcGFyYW09"
 	elif [ "${ssrss}" = "n" ]; then
 		echo "---"
 	fi
 elif [ "${howard}" = "2" ]; then
-	read -t 30 -p "是否卸载v2ray? y or n :" vtworaydel
+	read -t 30 -p "${Red_font_prefix}是否卸载v2ray?${Font_color_suffix} y or n :" vtworaydel
 	if [ "${vtworaydel}" = "y" ]; then
 		docker stop v2ray_mkcp v2ray_tcp 
 		docker rm v2ray_mkcp v2ray_tcp 
 		rm -f /etc/v2ray/*
-		echo "已卸载v2ray"
+		echo "${Red_font_prefix}已卸载v2ray${Font_color_suffix}"
 	elif [ "${vtworaydel}" = "n" ]; then
 		echo "---"
 	fi
-	read -t 30 -p "是否卸载ssr和ss? y or n :" ssrssdel
+	read -t 30 -p "${Red_font_prefix}是否卸载ssr和ss?${Font_color_suffix} y or n :" ssrssdel
 		if [ "${ssrssdel}" = "y" ]; then
 		docker stop shadowsocks-libev
 		docker rm shadowsocksR-libev
-		echo "已卸载ssr和ss"
+		echo "${Red_font_prefix}已卸载ssr和ss${Font_color_suffix}"
 	elif [ "${ssrssdel}" = "n" ]; then
 		echo "---"
 	fi
 elif [ "${howard}" = "3" ]; then
-	echo "goodbye"
+	echo "${Red_font_prefix}goodbye${Font_color_suffix}"
 fi
-rm v2rayinstall.sh
+rm -- "$0"
