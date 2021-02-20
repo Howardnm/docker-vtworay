b1938e48e98392d61896ce8b04e5d9505d7fcc5e
Howardnm <55622355+Howardnm@users.noreply.github.com>
2021-02-20 21:10:37 +0800

    Update v2rayinstall.sh

--- v2rayinstall.sh
+++ v2rayinstall.sh
@@ -9,3 +9,5 @@ wget -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/conf
 wget -P /etc/v2ray/ https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp.json
 docker run -d --name v2ray_mkcp --restart=always -v /etc/v2ray:/etc/v2ray -p 52001:52001 -p 52001:52001/udp v2fly/v2fly-core  v2ray -config=/etc/v2ray/config_mkcp.json
 docker run -d --name v2ray_tcp --restart=always -v /etc/v2ray:/etc/v2ray -p 51001:51001 -p 51001:51001/udp v2fly/v2fly-core  v2ray -config=/etc/v2ray/config_tcp.json
+elif [ "${isMove}" = "n" ]; then
+echo "已退出 !"
