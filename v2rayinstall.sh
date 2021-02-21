4f134bd473d7547edae9b70f9e076bfb094e7062
Howardnm <55622355+Howardnm@users.noreply.github.com>
2021-02-22 01:39:23 +0800

    Update v2rayinstall.sh

--- v2rayinstall.sh
+++ v2rayinstall.sh
@@ -14,7 +14,7 @@ mkdir /etc/v2ray
 	sleep 1
     fi
 clear
-    echo -e "  一键安装v2ray脚本 howard v2.2
+    echo -e "  一键安装v2ray脚本 howard v2.3
 ————————————————————————————————
  ${Green_font_prefix}1.${Font_color_suffix} 安装 v2ray ssr ss
  ${Green_font_prefix}2.${Font_color_suffix} 卸载 v2ray ssr ss
