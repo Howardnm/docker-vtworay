64c1a6712f1c177984934a467d8a636d265d66d9
Howardnm <55622355+Howardnm@users.noreply.github.com>
2021-02-22 02:06:53 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-awk '/port/' /etc/v2ray/config_tcp.json>/etc/v2ray/config_tcp_port.json
-awk '{print $2}' /etc/v2ray/config_tcp_port.json>/etc/v2ray/config_tcp_port2.json
-var=$(cat /etc/v2ray/config_tcp_port2.json)
-echo ${var}
