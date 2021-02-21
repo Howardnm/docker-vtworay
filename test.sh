f28ff5352a6db9f6a7e2227319f295c614192176
Howardnm <55622355+Howardnm@users.noreply.github.com>
2021-02-21 11:26:00 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
-url="https://raw.githubusercontent.com/Howardnm/vtworay/main/config_mkcp.json"
-echo ${url#https://}
-awk '/port/' /etc/v2ray/config_tcp.json>/etc/v2ray/config_tcp1.json
-awk '{print $2}' /etc/v2ray/config_tcp1.json>/etc/v2ray/config_tcp2.json
-var=$(cat somefile.txt)
+awk '/port/' /etc/v2ray/config_tcp.json>/etc/v2ray/config_tcp_port.json
+awk '{print $2}' /etc/v2ray/config_tcp_port.json>/etc/v2ray/config_tcp_port2.json
+var=$(cat /etc/v2ray/config_tcp_port2.json)
 echo ${var}
