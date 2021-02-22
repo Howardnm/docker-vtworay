67a6402694216534ca4f900dcf14ec57f88e1703
Howardnm <55622355+Howardnm@users.noreply.github.com>
2021-02-22 11:18:07 +0800

    Update v2rayinstall.sh

--- v2rayinstall.sh
+++ v2rayinstall.sh
@@ -51,20 +51,18 @@ clear
 	if [ "${vtworay}" = "y" ]; then
 	echo -e "————————————————————————————————
 vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogInYycmF5LWttY3AiLA0KICAiYWRkIjogIjAuMC4wLjAiLA0KICAicG9ydCI6ICI1MjAwMSIsDQogICJpZCI6ICI5YWM5YTBiMy1lODBiLTQ2NmItODEzYS03MzhjNmNmZDQ2Y2QiLA0KICAiYWlkIjogIjIwIiwNCiAgIm5ldCI6ICJrY3AiLA0KICAidHlwZSI6ICJub25lIiwNCiAgImhvc3QiOiAiIiwNCiAgInBhdGgiOiAiIiwNCiAgInRscyI6ICIiLA0KICAic25pIjogIiINCn0=
-
 vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogInYycmF5LXRjcCIsDQogICJhZGQiOiAiMC4wLjAuMCIsDQogICJwb3J0IjogIjUxMDAxIiwNCiAgImlkIjogIjlhYzlhMGIzLWU4MGItNDY2Yi04MTNhLTczOGM2Y2ZkNDZjZCIsDQogICJhaWQiOiAiMjAiLA0KICAibmV0IjogInRjcCIsDQogICJ0eXBlIjogIm5vbmUiLA0KICAiaG9zdCI6ICIiLA0KICAicGF0aCI6ICIiLA0KICAidGxzIjogIiIsDQogICJzbmkiOiAiIg0KfQ==
 ————————————————————————————————"
 	fi
 	if [ "${ssrss}" = "y" ]; then
 	echo -e "————————————————————————————————
 ss://YWVzLTEyOC1nY206aHhjMTIzQDAuMC4wLjA6NTQwMTk#ss
-
 ssr://MC4wLjAuMDo1NDAwOTpvcmlnaW46YWVzLTEyOC1jZmI6cGxhaW46YUhoak1USXovP3JlbWFya3M9YzNOeSZwcm90b3BhcmFtPSZvYmZzcGFyYW09
 ————————————————————————————————"
 	fi
-echo -n "订阅后，将ip改为" ; echo $wanip ; echo "即可使用"
+echo -n "订阅后，客户端将ip改为" ; echo $wanip
 echo -e "
-goodbye friends！！！
+即可使用，或者到网页扫码订阅：https://github.com/Howardnm/vtworay
 ————————————————————————————————
 docker正在运行的容器：
 ————————————————————————————————"
